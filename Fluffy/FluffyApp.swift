aa1ed5695ef56e3c6ef341b20fe6b98ff51ba78b
coffegorio <coffegorio@vk.com>
2026-04-23 11:28:15 +0300

    Initial Commit

---
+++ Fluffy/FluffyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluffyApp.swift
+//  Fluffy
+//
+//  Created by Egor Matveev on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FluffyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
